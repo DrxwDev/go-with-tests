93b2d2857027d01cba818b613fc07086af52771f
DrxwDev <drxwdev@outlook.es>
2025-12-16 20:03:37 -0400

    update: Arrays section
    Finish the arrays section learning about pre-allocation

---
+++ arrays/sum_test.go
@@ -0,0 +1,41 @@
+package arrays
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got: %d, want: %d, given: %v", got, want, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("go: %d, want: %d, given: %v", got, want, numbers)
+		}
+	})
+}
+
+func BenchmarkSum(b *testing.B) {
+	for b.Loop() {
+		Sum([]int{1, 2, 3, 4, 5})
+	}
+}
+
+func ExampleSum() {
+	sum := Sum([]int{1, 2, 3, 4, 5})
+	fmt.Println(sum)
+	// Output: 15
+}
